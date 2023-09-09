51ddd363ff8fba86bf691211c82cc5e19ac7ef2d
huan_kong <49610758+huankong233@users.noreply.github.com>
2023-09-10 00:32:16 +0800

    match the version

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
 <script src="https://unpkg.com/element-plus"></script>
 <script src="//unpkg.com/@element-plus/icons-vue"></script>-->
 
-<script src="https://cdn.bootcdn.net/ajax/libs/axios/1.3.6/axios.min.js"></script>
-<script src="https://cdn.bootcdn.net/ajax/libs/vue/3.2.47/vue.global.min.js"></script>
-<link href="https://cdn.bootcdn.net/ajax/libs/element-plus/2.3.3/index.min.css" rel="stylesheet">
-<script src="https://cdn.bootcdn.net/ajax/libs/element-plus/2.3.3/index.full.min.js"></script>
+<script src="https://cdn.bootcdn.net/ajax/libs/axios/1.5.0/axios.min.js"></script>
+<script src="https://cdn.bootcdn.net/ajax/libs/vue/3.3.4/vue.global.min.js"></script>
+<link href="https://cdn.bootcdn.net/ajax/libs/element-plus/2.3.12/index.min.css" rel="stylesheet">
+<script src="https://cdn.bootcdn.net/ajax/libs/element-plus/2.3.12/index.full.min.js"></script>
 <script src="https://cdn.bootcdn.net/ajax/libs/element-plus-icons-vue/2.1.0/global.iife.min.js"></script>
